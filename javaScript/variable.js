41f11772d38624b423e626f4b8a454025e957451
Binodsyangtan <binodsyangtan9761@gmail.com>
2024-01-07 22:00:44 +0545

    class update

---
+++ javaScript/variable.js
@@ -0,0 +1,46 @@
+// alert("javascript is conneted")
+console.log("javascript connected")
+console.log("hello world")
+
+
+
+/* multilevel comment garna(alt+shift+a) */
+
+
+/*
+     naming conventions
+     snake_Case
+     camelCase
+     
+     kebab-case  //only in css
+*/
+
+
+/*
+---data types--
+     
+     - string
+     - number
+     - boolean 
+     - null   ---jani jani khali xodeko ho null vaneko
+     - undefined      let price; undefined data defined xaina
+
+     // collection
+      1.array
+          collection of multiple values
+          always starts with index 0
+          /* 
+          syntax
+               let <variableName> = [<element1>, <element2>]
+           */
+
+let friend1 = "abc"
+let friend2 = "cxc"
+let friend3 = "avc"
+
+/* do not repeat yourself */
+//array bata banako
+//collection of multiple value
+
+let friends = ["binod", "rajkumar", "kumar"]
+console.log(friends);
