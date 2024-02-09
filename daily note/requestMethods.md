4ceb54ed458371ad901c6fb9fa520fb41bcaa430
Binodsyangtan <binodsyangtan9761@gmail.com>
2024-02-09 21:31:47 +0545

    todos-list

---
+++ daily note/requestMethods.md
@@ -0,0 +1,20 @@
+
+
+CRUD operations in database 
+    -Create
+    -Read
+    -Update
+    -DELETE
+
+Request Methods
+    - GET
+    - POST   -create
+    - put    - update
+    - DELETE
+
+STATUS CODES
+    - 2 SUCCESS
+    - 3 REDIRECT
+    - 4 CLIENT SIDE ERROR  -FRONTEND
+    - 5 BACKEND ERROR
+
--- javaScript/DOM-manipulation/todos/todos.js
+++ javaScript/DOM-manipulation/todos/todos.js
@@ -7,33 +7,42 @@ function addTodo(event) {
   let input = document.getElementById("input-title").value;
   // console.log("add", input)
   todos.push(input);
+  convertTodoList();
 
-  let outputlist = "";
-
-  todos.forEach((input, index) => {
-    outputlist += `<li>${input} <button onclick="deleteTodo(${index})">delete</button></li> `
-  });
-  /* delete garda only jun delete garni tyo matra delete hunu paryou */
-
-  console.log(outputlist);
-  document.getElementById("todos-list").innerHTML = outputlist;
-};
-
-function deleteTodo(deletableindex){
-  todos = todos.filter((el,index) => index != deletableindex)
-
-
-  let outputlist = "";
-
-  todos.forEach((input, index) => {
-    outputlist += `<li>${input} <button onclick="deleteTodo(${index})">delete</button></li> `
-  });
-  /* delete garda only jun delete garni tyo matra delete hunu paryou */
-
-  console.log(outputlist);
-  document.getElementById("todos-list").innerHTML = outputlist;
   
+}
+
+function deleteTodo(deletableindex) {
+  /* first kun delete garni teslai print garna sikne like console garera kun index lai delete garne telslai parint garna sikne first */
+  todos = todos.filter((el, index) => index != deletableindex);
+
+  convertTodoList();
+
+  // let outputlist = "";
+
+  // todos.forEach((input, index) => {
+  //   outputlist += `<li>${input} <button onclick="deleteTodo(${index})">delete</button></li> `;
+  // });
+  // /* delete garda only jun delete garni tyo matra delete hunu paryou */
+
+  // console.log(outputlist);
+  // document.getElementById("todos-list").innerHTML = outputlist;
+}
 
 
+
+/* repeat vako xa hamle repeat harule yak thau mah rakhera call garna sakinxa */
+
+function convertTodoList(){
+
+  let outputlist = "";
+
+  todos.forEach((input, index) => {
+    outputlist += `<li>${input} <button onclick="deleteTodo(${index})">delete</button></li> `;
+  });
+  /* delete garda only jun delete garni tyo matra delete hunu paryou */
+
+  console.log(outputlist);
+  document.getElementById("todos-list").innerHTML = outputlist;
 
 }
